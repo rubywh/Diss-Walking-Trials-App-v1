d48776af8cde5eded59d6c5f242f081ffd9960fb
rubywh <ruby_whipp@hotmail.co.uk>
2016-12-22 21:34:56 +0000

    initial commit

---
+++ app/src/test/java/ruby/trialproject/WearableServiceTest.java
@@ -0,0 +1,34 @@
+package ruby.trialproject;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ruby__000 on 22/12/2016.
+ */
+public class WearableServiceTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnSensorChanged() throws Exception {
+
+    }
+
+    public void testOnDestroy() throws Exception {
+
+    }
+
+    public void testOnAccuracyChanged() throws Exception {
+
+    }
+
+    public void testGetFile() throws Exception {
+
+    }
+
+}
